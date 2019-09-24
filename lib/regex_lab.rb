484090d1a119de52c42a232dc2cb4f67f367720c
Daniel Matney <danieljmatney@gmail.com>
2019-09-24 16:46:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\A\d{10}\z/)
+  if phone.match(/\A\D\d{3}\D\z/)
     true
   else false
   end
